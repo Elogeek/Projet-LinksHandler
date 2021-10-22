a561d103cef95bccff32faee28cf650f95b393b9
elogeek <elodiechristin@gmail.com>
2021-10-22 12:41:06 +0200

    suite projet

--- view/links.view.php
+++ view/links.view.php
@@ -1,5 +1,4 @@
 
-
     <div class="linkImage">
         <div class="image"><img src="/public/assets/image/imgLinks.webp" alt="Image"></div>
         <div class="linkName">Link name</div>
--- view/login.view.php
+++ view/login.view.php
@@ -1,15 +1,19 @@
+<div class="windowConnect">
+    <div id="loginDiv">
+        <form action="../public/index.php?controller=user&action=login" method="POST">
+            <div>
+                <label for="mail">Adresse mail: </label>
+                <input type="email" name="mail" id="mail" required>
+            </div>
+            <div>
+                <label for="password">Mot de passe: </label>
+                <input type="password" name="password" id="password" required>
+            </div>
 
-<div id="loginDiv">
-    <form action="../public/index.php?controller=user&action=login" method="POST">
-        <div>
-            <label for="mail">Adresse mail:</label>
-            <input type="email" name="mail" id="mail" required>
-        </div>
-        <div>
-            <label for="password">Mot de passe:</label>
-            <input type="password" name="password" id="password" required>
-        </div>
-        <input type="submit" id="buttonSubmit" value="Se connecter">
-    </form>
-    <div class="backButton"><a href="../public/index.php">Retour</a></div>
+            <input type="submit" id="buttonSubmit" value="Se connecter">
+            <div class="backButton"><a href="../public/index.php">Retour</a></div>
+
+        </form>
+
+    </div>
 </div>
