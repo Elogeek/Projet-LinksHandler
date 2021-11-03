a228c021eaceec91c781d8b5f5404ff9e74ecb40
elogeek <elodiechristin@gmail.com>
2021-11-03 12:04:21 +0100

    test debugg project

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
@@ -22,19 +23,16 @@ if ((!isset($_SESSION['id']) || $_SESSION['id'] !== true) && !isset($_POST['logi
 
         switch ($controller) {
 
-            // switch les actions de LinkController via la fct chooseLinksControllerAction()
-
             case 'link':
                 $controller = new LinkController();
                 if (isset($_GET['action'])) {
                     chooseLinksControllerAction($controller);
-
                 } else {
                     $controller->homeLinks();
                 }
                 break;
 
-            // Connect or disconnect a user via la fct routeUser()
+            // Connect or disconnect a user via la fct routeUser
             case 'user':
                 if (isset($_GET['action'])) {
                     $controller = new UserController();
@@ -43,57 +41,59 @@ if ((!isset($_SESSION['id']) || $_SESSION['id'] !== true) && !isset($_POST['logi
                 }
 
             default:
-                $controller = new HomeController();
-                $controller->showHome();
-        }
-    } else {
-        (new LinkController())->homeLinks();
-    }
-
-    /**
-     * Choose the right action to call from links controller.
-     */
-    function chooseLinksControllerAction(LinkController $controller) {
-
-        switch (filter_var($_GET['action'], FILTER_SANITIZE_STRING)) {
-            // Add a link
-            case 'add':
-                $controller->add();
-                break;
-            // Update a link
-            case 'update' :
-                if (isset($_GET['id'])) {
-                    $controller->update((int)$_GET['id']);
-                }
-                break;
-            //Delete a link
-            case 'delete' :
-                if (isset($_GET['id'])) {
-                    $controller->delete((int)$_GET['id']);
-                }
-                break;
-            // home links
-            default :
+                $controller = new LinkController();
                 $controller->homeLinks();
         }
-    }
-
-
-    /**
-     * If connect / if disconnect a user
-     * @param UserController $controller
-     */
-    function routeUser(UserController $controller) {
-
-        switch (filter_var($_GET['action'], FILTER_SANITIZE_STRING)) {
-            case 'logout' :
-                $controller->logout();
-                break;
-            default :
-                $controller->login();
-
-        }
+    } else {
+        $controller = new LinkController();
+        $controller->homeLinks();
 
     }
 }
 
+
+/**
+ * Choose the right action to call from links controller.
+ */
+function chooseLinksControllerAction(LinkController $controller) {
+
+    switch (filter_var($_GET['action'], FILTER_SANITIZE_STRING)) {
+        // Add a link
+        case 'add':
+            $controller->add();
+            break;
+        // Update a link
+        case 'update' :
+            if (isset($_GET['id'])) {
+                $controller->update((int)$_GET['id']);
+            }
+            break;
+        //Delete a link
+        case 'delete' :
+            if (isset($_GET['id'])) {
+                $controller->delete((int)$_GET['id']);
+            }
+            break;
+        // home links
+        default :
+            $controller->homeLinks();
+    }
+}
+
+
+/**
+ * If connect / if disconnect a user
+ * @param UserController $controller
+ */
+function routeUser(UserController $controller) {
+
+    switch (filter_var($_GET['action'], FILTER_SANITIZE_STRING)) {
+        case 'logout' :
+            $controller->logout();
+            break;
+        default :
+            $controller->login();
+
+    }
+
+}
