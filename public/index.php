b587b40a8f75ae57e605cc2dcd0fde929b18991f
elogeek <elodiechristin@gmail.com>
2021-11-03 12:04:52 +0100

    test debugg project

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-use Elogeek\LinksHandler\Controller\HomeController;
 use Elogeek\LinksHandler\Controller\LinkController;
 use Elogeek\LinksHandler\Controller\UserController;
 use Elogeek\LinksHandler\Model\Entity\User;
