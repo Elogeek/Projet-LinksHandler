8a87cac68ddbb87bad78496c5ec138f747fa1ead
elogeek <elodiechristin@gmail.com>
2021-11-05 18:58:20 +0100

    suite project

---
+++ _docs/vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a739b0ac7c2d995c540e3614be23609b2cb7d5a8',
+        'name' => 'upto/projets',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'muffeen/url-status' => array(
+            'pretty_version' => '0.2.0',
+            'version' => '0.2.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../muffeen/url-status',
+            'aliases' => array(),
+            'reference' => 'a3565f7caf0528e320771b9d33f1e514019a4e42',
+            'dev_requirement' => false,
+        ),
+        'roave/security-advisories' => array(
+            'pretty_version' => 'dev-latest',
+            'version' => 'dev-latest',
+            'type' => 'metapackage',
+            'install_path' => NULL,
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => '0488e161600117fc3a0d72397dad154729002f54',
+            'dev_requirement' => true,
+        ),
+        'upto/projets' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a739b0ac7c2d995c540e3614be23609b2cb7d5a8',
+            'dev_requirement' => false,
+        ),
+    ),
+);
